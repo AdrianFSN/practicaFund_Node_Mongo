bc3060f8eefaa612b8b2b5b6af43f29b98be80e3
Adrián S <adriansotes@gmail.com>
2024-02-28 19:24:14 +0100

    fix: consigo llevarme el método para sacar la lista principal a un módulo separado: mainList.js

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,52 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const AdNopop = require('../models/AdNodepop');
+const adsRouter = require('./mainList');
 const tagsRouter = require('./tags');
 
-/* GET home page. */
-router.get('/', async function (req, res, next) {
-  try {
-    // filters
-    const filterByTag = req.query.tag;
-    const filterByName = req.query.name;
-    const filterByOnSale = req.query.onSale;
-    const filterByPrice = req.query.price;
 
-    //paging
-    const skip = req.query.skip;
-    const limit = req.query.limit;
-
-    //ordering
-    const sort = req.query.sort;
-
-    //fields selection
-    const fields = req.query.fields;
-
-    const filter = {};
-
-    if (filterByTag) {
-      filter.tag = filterByTag;
-    }
-    if (filterByName) {
-      filter.name = new RegExp('^' + filterByName, "i");
-    }
-    if (filterByOnSale) {
-      filter.onSale = filterByOnSale;
-    }
-    if (filterByPrice) {
-      filter.price = filterByPrice;
-    }
-
-    const adsList = await AdNopop.listCriterias(filter, skip, limit, sort, fields);
-    res.render('index', { title: 'Nodepop', adsList: adsList });
-
-  } catch (error) {
-    next(error);
-  }
-});
 
 //Router to get the main list:
-
+router.use('/', adsRouter);
 // Router to get the tags list:
 router.use('/tags', tagsRouter);
 
