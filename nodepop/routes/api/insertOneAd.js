2233a201cc0c0d9a4b188320de048d510f80eae5
Adrián S <adriansotes@gmail.com>
2024-03-02 14:32:54 +0100

    feat: validaciones en todos los métodos de la api y del frontend

--- nodepop/routes/api/insertOneAd.js
+++ nodepop/routes/api/insertOneAd.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const AdNopop = require('../../models/AdNodepop');
-const { query, validationResult } = require('express-validator');
 
 // POST /api/adsNodepop/insert (body)
 // Insert a new add
--- nodepop/routes/api/updateAd.js
+++ nodepop/routes/api/updateAd.js
@@ -1,21 +1,42 @@
+'use strict'
+
 var express = require('express');
 var router = express.Router();
 const AdNopop = require('../../models/AdNodepop');
-const { query, validationResult } = require('express-validator');
+const { body, param, validationResult } = require('express-validator');
+const fs = require('fs');
 
 // PUT /api/adsNodepop/update/<_id> (body)
 // updates an ad
-router.put('/:id', async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const data = req.body;
-        const updatedAd = await AdNopop.findByIdAndUpdate(id, data, { new: true });
+router.put('/:id',
+    [
+        param('id').isMongoId().withMessage('The ID you introduced does not exist or is not in a valid format.'),
+        body('name').notEmpty().withMessage('Name cannot be empty.'),
+        body('sale').notEmpty().withMessage('Sale must be boolean: "true" (for selling) or "false" (for buying).'),
+        body('price').isNumeric().withMessage('You must define a price in number for your offer'),
+        body('tag').custom(value => {
+            const jsonTagsList = fs.readFileSync('./data/tagsList.json', 'utf-8');
+            const tagsList = JSON.parse(jsonTagsList);
+            const availableTags = tagsList.results;
 
-        res.json({ result: updatedAd });
+            if (availableTags.includes(value))
+                return true;
+        }
+        ).withMessage('Tags can only be "lifestyle", "mobile", "work" or "motor" (lowercased).')
+    ],
+    async (req, res, next) => {
+        try {
+            validationResult(req).throw();
 
-    } catch (error) {
-        next(error);
-    };
-});
+            const id = req.params.id;
+            const data = req.body;
+            const updatedAd = await AdNopop.findByIdAndUpdate(id, data, { new: true });
+
+            res.json({ result: updatedAd });
+
+        } catch (error) {
+            next(error);
+        };
+    });
 
 module.exports = router;
